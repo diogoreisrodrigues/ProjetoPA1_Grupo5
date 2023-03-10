8f016a1a3c40e76837de47e19aceb187d68fd7ab
[VitorVieira20] <[2078320@student.uma.pt]>
2023-03-10 15:50:39 +0000

    feat[maxClients] : added a max capacity to server and a waiting list

--- server/src/main/java/ServerThread.java
+++ server/src/main/java/ServerThread.java
@@ -3,9 +3,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.AbstractExecutorService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class ServerThread extends Thread {
     private final int port;
@@ -13,13 +18,16 @@ public class ServerThread extends Thread {
     private PrintWriter out;
     private ServerSocket server;
     private Socket socket;
-
     private final ExecutorService executor;
-
+    private static final Logger logger = Logger.getLogger(ServerThread.class.getName());
+    private int maxClients;
+    private final Semaphore semaphore;
 
     public ServerThread ( int port ) {
         this.port = port;
-        this.executor = Executors.newFixedThreadPool(9);         //Por agora nthread ta um numero fixo mas depois corrigir para ficar dinâmico
+        this.maxClients = 2;
+        this.semaphore = new Semaphore( maxClients );
+        this.executor = Executors.newFixedThreadPool( maxClients );         //Por agora nthread ta um numero fixo mas depois corrigir para ficar dinâmico
         try {
             server = new ServerSocket ( this.port );
         } catch ( IOException e ) {
@@ -32,15 +40,9 @@ public class ServerThread extends Thread {
      */
     public void run ( ) {
         try {
+            logger.info("Server started");
             System.out.println ( "Accepting Data" );
             acceptClient();
-                /*
-                in = new DataInputStream ( socket.getInputStream ( ) );
-                out = new PrintWriter ( socket.getOutputStream ( ) , true );
-                String message = in.readUTF ( );
-                System.out.println ( "***** " + message + " *****" );
-                out.println ( message.toUpperCase ( ) );
-                */
         } catch ( IOException e ) {
             throw new RuntimeException();
         }
@@ -48,15 +50,36 @@ public class ServerThread extends Thread {
     }
 
     private void acceptClient() throws IOException {
-
+        FileHandler fh;
+        fh = new FileHandler("server.log");
+        SimpleFormatter formatter = new SimpleFormatter();
+        fh.setFormatter(formatter);
+        Queue<Socket> waitingClientsList = new LinkedList<>();
         Thread t = new Thread(() -> {
             while( true ){
                 try {
-                    socket = server.accept ( );
-                    ClientWorker clientWorker = new ClientWorker(socket);     //Estou a criar
-                    executor.submit(clientWorker);
-                } catch(IOException e){
+                    semaphore.acquire( );
+                    if ( ((ThreadPoolExecutor)executor).getActiveCount() < (maxClients - 1) ){
+                        socket = server.accept ( );
+                        ClientWorker clientWorker = new ClientWorker(socket, logger);     //Estou a criar
+                        executor.submit( clientWorker );
+                    } else {
+                        waitingClientsList.offer( socket );
+                    }
+                } catch(IOException | InterruptedException e ){
                     throw new RuntimeException();
+                } finally {
+                    semaphore.release();
+                }
+                while ( !waitingClientsList.isEmpty() ){
+                    Socket waitingSocket = waitingClientsList.peek();
+                    if (((ThreadPoolExecutor) executor).getActiveCount() < (maxClients - 1) ) {
+                        waitingClientsList.poll();
+                        ClientWorker clientWorker = new ClientWorker(waitingSocket, logger);
+                        executor.submit(clientWorker);
+                    } else {
+                        break;
+                    }
                 }
             }
         });
