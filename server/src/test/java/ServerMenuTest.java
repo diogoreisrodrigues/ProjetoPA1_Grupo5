e62203992476df7d1c81fbf0a53021a2229042b3
ftferreira02 <tomasfreitas4054@gmail.com>
2023-03-26 20:48:18 +0100

    test[ServerMenu]: added some tests to Server Menu

---
+++ server/src/test/java/ServerMenuTest.java
@@ -0,0 +1,96 @@
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServerMenuTest {
+
+    private Logger logger;
+    private ServerMenu sm;
+    private MySemaphore semaphore;
+    private int maxClients;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        this.sm = new ServerMenu(logger,maxClients,semaphore);
+    }
+
+    @Test
+    public void testWordInFile() {
+        assertAll(
+                () -> assertTrue(sm.wordInFile("bannedWords.txt", "hello")),
+                () -> assertFalse(sm.wordInFile("bannedWords.txt", "casa"))
+        );
+        assertThrows(RuntimeException.class, () -> {
+            sm.wordInFile("invalidFileName.txt", "hello");
+        });
+    }
+
+    @Test
+    public void testAddWordToFile() throws IOException {
+
+        //Caso em que o ficheiro está vazio
+
+        File tempFile = File.createTempFile("testFile", ".txt");
+        sm.addWordToFile(tempFile.getPath(), "test");
+
+        BufferedReader reader = new BufferedReader(new FileReader(tempFile));
+        List<String> lines = reader.lines().toList();
+        reader.close();
+
+        assertEquals(lines.size(), 1);
+        assertTrue(lines.get(0).contains("test"));
+
+        tempFile.delete();
+
+        //Caso em que o ficheiro já tem conteúdo
+
+        File tempFile1 = File.createTempFile("testFile1", ".txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile1));
+        writer.write("test");
+        writer.close();
+        sm.addWordToFile(tempFile1.getPath(), "test1");
+
+        BufferedReader reader1 = new BufferedReader(new FileReader(tempFile1));
+        List<String> lines1 = reader1.lines().toList();
+        reader1.close();
+        tempFile1.delete();
+
+        assertEquals(lines1.size(), 2);
+        assertTrue(lines1.get(1).contains("test1"));
+        assertThrows(RuntimeException.class, () -> sm.addWordToFile(tempFile.getPath(), "world"));
+
+    }
+
+    @Test
+    public void testRemoveWordsfromFile() throws IOException {
+
+    }
+
+    @Test
+    void testAddWordsToMenu() {
+        /*
+        //1º Adicionar nova palavra
+        String input = "testWord";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        sm.addWordToFile("testFile.txt",input);
+        assertTrue(sm.wordInFile("testFile.txt", input));
+
+        //2ºPalavra ja existente no ficheiro
+        input="hello";
+        sm.addWordToFile("testFile.txt",input);
+        sm.addWordsToMenu("testFile.txt");
+        assertTrue(sm.wordInFile("testFile.txt", input));
+        */
+
+    }
+
+
+}
